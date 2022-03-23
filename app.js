38e9aec9ded19643bc57bd0427d9c87d65140e7b
mjaichand <mjaichandms@gmail.com>
2022-03-22 21:10:38 -0500

    Updated app.js
    part4 completed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const server =
     (req,res)=> {               //Anonymous function to handle the request
       res.statusCode = 200      //code for OK
       res.setHeader('Content-Type', 'text/plain') //Set the mime type
-      res.end('Hello World')    //Close the response and provide content
+      res.end('Fun Fact about me: I always fall in love with myself')    //Close the response and provide content
     }                           //No return needed, we modified the res object we got
 )
 
