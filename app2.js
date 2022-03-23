603830304a383e3765b489befbd301093024314d
mjaichand <mjaichandms@gmail.com>
2022-03-23 10:14:31 -0500

    updated files

--- app2.js
+++ app2.js
@@ -11,9 +11,16 @@ const server =
 
       res.write('<html> <head> <title> Served </title> </head>')
       res.write('<body>')
-      res.write('Content \n')
-      res.write('More content \n')
-      res.write('Hello World')    
+      res.write('Welcome to my World')
+      res.write('<h3>Sports Activity</h3>')
+      res.write('<p> Below Table gives an overview for participants to choose a particular Sport/Activity based on their interest. The Name column represents the name of the sport/activity. The Location column shows the location where to participate in the sport/activity. The Equipment Fee column shows the amount of money for personal equipment. </p>')
+      res.write('<table>')
+      res.write('<tr> <th>Name</th> <th>Location</th> <th>Equipment Fee</th> </tr>')
+      res.write('<tr> <td>Football</td> <td>next to Colden Hall</td> <td>20$</td> </tr>')
+      res.write('<tr> <td>Criket</td> <td>near Union Ball Room</td> <td>30$</td> </tr>')
+      res.write('<tr> <td>TableTennis</td> <td>opp. Horizon West</td> <td>5$</td> </tr>')
+      res.write('<tr> <td>Basketball</td> <td>Fitness Centre</td> <td>10$</td> </tr>')
+      res.write('</table>')
       res.end('</body></html>')
       //Close the response
     }                           
---
+++ compute.js
@@ -0,0 +1,49 @@
+const http = require('http')    //Pull in a useful node package
+
+                                
+const hostname = process.env.hostname || '127.0.0.1'    //get our ip address from the environment
+const port = process.env.port || 3001               //and the port
+const server =
+  http.createServer(            //Creates the response loop
+    (req,res)=> {               //Anonymous function to handle the request
+      console.log("Request recieved")
+      console.log(req.url)
+      // Build a fake url so we can get the search parameters using a URL object
+      fake_url = "https://fake.com/path" + req.url
+      const url = new URL(fake_url)
+      const search_params = url.searchParams
+      const x=search_params.get("x")
+      const y=search_params.get("y")
+      const fx = Math.fround(x)
+      const fy = Math.fround(y)
+      const max = Math.max(x,y)
+      const min = Math.min(x,y)
+      function getRandomArbitrary(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+      }
+      const gxy = getRandomArbitrary(min, max)
+      console.log("Queries: " + search_params)
+
+      if (req.method === 'GET' ) {
+       
+          console.log("Look for query parameter x: " + search_params.get("x"))
+          console.log("Look for query parameter y: " + search_params.get("y"))
+          // Process the queries here
+          res.statusCode = 200      //code for OK
+          res.setHeader('Content-Type', 'text/plain')
+          res.write("\"f = uses Math.fround & g = uses Math.random\"\n")
+          res.write(`f(${x}) is ${fx}, f(${y}) is ${fy} and g(${x},${y}) is ${gxy}`)
+          res.end();
+        
+      } else {
+        console.log("Status 404")
+        res.statusCode = 404;
+        res.end();
+      }
+
+    }                           
+)
+
+server.listen(port, hostname, () => {   //Start the server
+  console.log(`Server running at http://${hostname}:${port}/`)  //Log the request
+})
